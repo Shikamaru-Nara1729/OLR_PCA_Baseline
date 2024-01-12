4b7da1c29da02562bda3d2e0f5a52bbcb256f404
Apoorva Dinesh Singh <34515315+Shikamaru-Nara1729@users.noreply.github.com>
2024-01-12 21:52:55 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+DATA_PATH = "/Users/apoorvasingh/Desktop/olr_baseline/datasets/OLR_1979_2019_MJJAS_30S_30N_80W_150E.npy"
+LABELS_PATH = "/Users/apoorvasingh/Desktop/olr_baseline/datasets/final_label_imd_jan2024.npy"
+
+WINDOW = 15
+YEARS = 40
+DAYS = 149
+LABELS = 135
